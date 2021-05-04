e41b9c42fc28a62d296fde3ec0d66df99ad103ea
MmeiyuC <>
2021-05-04 16:18:25 +0000

    added comments

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let side = 0
 let area = side * side
 
+// This is a code for the button:
 document.getElementById('calculate').addEventListener('click', areaofsquare)
 
 function areaofsquare () {
   side = document.getElementById('side').value
   side = parseInt(side)
-  area = side * side
+  area = side * side // This is the calculation of the area of square
   alert(area)
 }
