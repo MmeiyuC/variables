8cfc63836951cfe3ad4d7c28c355fa5a35a642f1
MmeiyuC <>
2021-05-04 15:38:58 +0000

    edited

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let side = 0
+let area = side * side
 
 document.getElementById('calculate').addEventListener('click', areaofsquare)
 
@@ -6,7 +7,7 @@ function areaofsquare () {
   side = document.getElementById('side').value
 
   side = parseInt(side)
-
+  
   area = side * side
 
   alert(area)
